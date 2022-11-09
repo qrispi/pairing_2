08779ce711a8cae915f1069e5c18a103824c14f3
Christopher Baum <christopherabaum@gmail.com>
2022-11-09 17:51:12 -0600

    Initial commit

---
+++ friends.js
@@ -0,0 +1,10 @@
+var bestFriend = "greg"
+var friend = "steve"
+
+if (bestFriend === "Matt") {
+    console.log("Hi, bestie!")
+} else if (friend === "Kyle") {
+    console.log("Hi, Kyle!")
+} else {
+    console.log("Hi, stranger!")
+}
