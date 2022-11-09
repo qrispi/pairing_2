b39458f9a36040d512a39654213a9dabc4bd40df
Christopher Baum <christopherabaum@gmail.com>
2022-11-09 17:55:45 -0600

    Add else if statement

--- friends.js
+++ friends.js
@@ -1,10 +1,14 @@
 var bestFriend = "greg"
 var friend = "steve"
+var anotherFriend = "Marina"
 
 if (bestFriend === "Matt") {
     console.log("Hi, bestie!")
 } else if (friend === "Kyle") {
     console.log("Hi, Kyle!")
-} else {
+} else if (anotherFriend === "Marina") {
+    console.log("Hi, Marina! You're a very good friend!")
+}
+    else {
     console.log("Hi, stranger!")
 }
