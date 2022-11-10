3e9a54f1c81ec19d200abce7f0d63572ce3b7992
Christopher Baum <christopherabaum@gmail.com>
2022-11-09 18:06:31 -0600

    Add a variation to the exercise

--- friends.js
+++ friends.js
@@ -1,14 +1,28 @@
-var bestFriend = "greg"
-var friend = "steve"
-var anotherFriend = "Marina"
+// var bestFriend = "greg"
+// var friend = "steve"
+// var anotherFriend = "Marina"
 
-if (bestFriend === "Matt") {
-    console.log("Hi, bestie!")
-} else if (friend === "Kyle") {
+// if (bestFriend === "Matt") {
+//     console.log("Hi, bestie!")
+// } else if (friend === "Kyle") {
+//     console.log("Hi, Kyle!")
+// } else if (anotherFriend === "Marina") {
+//     console.log("Hi, Marina! You're a very good friend!")
+// } else {
+//     console.log("Hi, stranger!")
+// }
+
+
+var friend = "Marina"
+var anotherFriend = "Kyle"
+
+
+if (friend === "Marina" && anotherFriend === "Kyle") {
+    console.log("Hi, besties!")
+} else if (anotherFriend === "Kyle") {
     console.log("Hi, Kyle!")
-} else if (anotherFriend === "Marina") {
+} else if (friend === "Marina") {
     console.log("Hi, Marina! You're a very good friend!")
-}
-    else {
+} else {
     console.log("Hi, stranger!")
 }
